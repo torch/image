c3e5792e3e1045a44dd365c0ff314ef5d9070687
Julien Cornebise <julien.cornebise@gmail.com>
2014-10-03 22:34:29 +0100

    Get ready to add more tests for vflip/hflip

--- tests/test_vflip.lua
+++ tests/test_vflip.lua
@@ -4,28 +4,28 @@ require 'image'
 myTests = {}
 local tester = totem.Tester()
 
-local function test_transformation(f)
+
+-- List of all possible tests
+local all_tests = {}
+function all_tests.test_transformation_largeByteImage(flip)
     local x_real = image.fabio():double():mul(255)
     local x_byte = x_real:clone():byte()
 
     assert(x_byte:size(1) > 256 and x_byte:size(2) > 256, 'Tricky case only occurs for images larger than 256 px, pick another example')
 
     local f_real, f_byte
-    f_real = image[f](x_real)
-    f_byte = image[f](x_byte)
-    tester:assertTensorEq(f_real:byte():double(), f_byte:double(), 1e-16, f .. ':  result for double and byte images do not match')
+    f_real = image[flip](x_real)
+    f_byte = image[flip](x_byte)
+    tester:assertTensorEq(f_real:byte():double(), f_byte:double(), 1e-16, flip .. ':  result for double and byte images do not match')
 end
 
 
-function myTests.test_vflip()
-    test_transformation('vflip')
+-- Apply all tests to both vflip and hflip
+for _, flip in pairs{'vflip', 'hflip'} do
+    for name, test in pairs(all_tests) do
+        myTests[name .. '_' .. flip] = function() return test(flip) end
+    end
 end
-
-
-function myTests.test_hflip()
-    test_transformation('hflip')
-end
-
 
 tester:add(myTests)
 return tester:run(myTests)
