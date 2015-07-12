14e9dad412b0dc86c4f09acb6134c59ab9511df3
Matthieu Tourne <matthieu.tourne@gmail.com>
2015-07-12 14:34:47 -0700

    Fixing broken PGMA loading - Adding test

--- test/test_ppm.lua
+++ test/test_ppm.lua
@@ -18,6 +18,24 @@ function myTests.test_ppmload()
     tester:assertTensorEq(pix, ref, 0, "PPM load: first pixel check failed")
 end
 
+
+function myTests.test_pgmaload()
+    -- ascii.ppm is a PGMA file (ascii pgm)
+    -- example comes from ehere
+    -- http://people.sc.fsu.edu/~jburkardt/data/pgma/pgma.html
+    local img = image.load(paths.concat(sys.fpath(), "ascii.pgm"), 1, 'byte')
+    local max_gray = 15 -- 4th line of ascii.pgm
+    local ascii_val = 3 -- pixel (2,2) in the file
+    local pix_val = math.floor(255 * ascii_val / max_gray)
+
+    local pix = img[{ {}, {2}, {2} }]
+
+    -- Check that Pixel(1, 2,2) == 3
+    local ref = torch.zeros(1, 1, 1)
+    ref[1][1][1] = pix_val
+    tester:assertTensorEq(pix, ref, 0, "PGMA load: pixel check failed")
+end
+
 function myTests.test_pgmload()
     -- test.ppm is a 100x1 "French flag" like image, i.e the first pixel is blue
     -- the 84 next pixels are white and the 15 last pixels are red.
