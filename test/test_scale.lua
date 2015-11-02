ba54371234e1f2b1d7a1360f5b02a747d0585f27
John Agapiou <jagapiou@google.com>
2015-11-02 18:13:45 +0000

    Fix bug with image.scale 'bicubic' and ByteTensors.
    Bicubic interpolation and scaling do not work for ByteTensors due to issues with
    numerical precision.
    
    * Add unit tests for ByteTensor scaling.
    * Update scaling to use existing cubic interpolation method.
    * Update cubic interpolation to use double precision Temporary variables.
    * Add function to handle casting from `double` to `real`.

--- test/test_scale.lua
+++ test/test_scale.lua
@@ -14,35 +14,64 @@ local function outerProduct(x)
 end
 
 
+local function assertTensorEq(actual, expected)
+  if torch.type(expected) == 'torch.ByteTensor' then
+    local areEqual = torch.eq(actual, expected):all()
+    tester:assert(areEqual)
+  else
+    tester:assertTensorEq(actual, expected, 1e-5)
+  end
+end
+
+
 function tests.bilinearUpscale()
   local im = outerProduct{1, 2, 4, 2}
   local expected = outerProduct{1, 1.5, 2, 3, 4, 3, 2}
-  local actual = image.scale(im, expected:size(1), expected:size(2), 'bilinear')
-  tester:assertTensorEq(actual, expected, 1e-5)
+  local actual = image.scale(im, expected:size(2), expected:size(1), 'bilinear')
+  assertTensorEq(actual, expected)
 end
 
 
 function tests.bilinearDownscale()
   local im = outerProduct{1, 2, 4, 2}
   local expected = outerProduct{1.25, 3, 2.5}
-  local actual = image.scale(im, expected:size(1), expected:size(2), 'bilinear')
-  tester:assertTensorEq(actual, expected, 1e-5)
+  local actual = image.scale(im, expected:size(2), expected:size(1), 'bilinear')
+  assertTensorEq(actual, expected)
 end
 
 
 function tests.bicubicUpscale()
   local im = outerProduct{1, 2, 4, 2}
   local expected = outerProduct{1, 1.4375, 2, 3.1875, 4, 3.25, 2}
-  local actual = image.scale(im, expected:size(1), expected:size(2), 'bicubic')
-  tester:assertTensorEq(actual, expected, 1e-5)
+  local actual = image.scale(im, expected:size(2), expected:size(1), 'bicubic')
+  assertTensorEq(actual, expected)
 end
 
 
 function tests.bicubicDownscale()
   local im = outerProduct{1, 2, 4, 2}
   local expected = outerProduct{1, 3.1875, 2}
-  local actual = image.scale(im, expected:size(1), expected:size(2), 'bicubic')
-  tester:assertTensorEq(actual, expected, 1e-5)
+  local actual = image.scale(im, expected:size(2), expected:size(1), 'bicubic')
+  assertTensorEq(actual, expected)
+end
+
+
+function tests.bicubicUpscale_ByteTensor()
+  local im = torch.ByteTensor{{0, 1, 32}}
+  local expected = torch.ByteTensor{{0, 0, 9, 32}}
+  local actual = image.scale(im, expected:size(2), expected:size(1), 'bicubic')
+  assertTensorEq(actual, expected)
+end
+
+
+function tests.bilinearUpscale_ByteTensor()
+  local im = torch.ByteTensor{{1, 2},
+                              {2, 3}}
+  local expected = torch.ByteTensor{{1, 1, 2},
+                                    {1, 1, 2},
+                                    {2, 2, 3}}
+  local actual = image.scale(im, expected:size(2), expected:size(1))
+  assertTensorEq(actual, expected)
 end
 
 
